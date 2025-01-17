61e00b9418667e7872ffbc1f173a72350ef5f0a0
BlueCollarGiant <167778887+BlueCollarGiant@users.noreply.github.com>
2025-01-16 19:34:36 -0600

    finished my in class homework

---
+++ script.js
@@ -0,0 +1,55 @@
+/* start of first*/
+/* let total = 5 + 2;
+let diffrence = 5 - 2;
+
+console.log(total);
+console.log(diffrence);*/
+/*end of first*/
+
+/* start of second*/
+
+/*let name = "isaiah";
+let age = 38;
+let favoriteColor = "blue";
+
+let greeting = `Hello my name is ${name} im ${age} my favorite color is ${favoriteColor} and im happy to be here coding with you today`;
+
+console.log(greeting)*/
+/*start of third */
+/* let originalPrice = 100;
+let tax = 0.07;
+let total = originalPrice - (originalPrice * tax);
+
+console.log(total)
+*/
+/* end of third */
+/* start of fourth */
+/*let firstName = "isaiah";
+let lastName = "ratliff";
+
+let sentence = `my name is ${firstName} ` + `${lastName} how are you?`;
+
+console.log(sentence) */
+/* finish fourth */
+/* start of fifth */
+/* let inventory = 100;
+let sold = 15;
+let restock = 20;
+
+let total = inventory - sold + restock;
+
+console.log(total) */
+/* end of fifth */
+/* start of sixth */
+let startPrice = 120;
+let taxRate = 0.1;
+let discount = 0.15;
+
+let pricewithTax = startPrice - (startPrice * taxRate);
+let finalPrice = pricewithTax - (startPrice * discount);
+
+let taxmessage = ` The Price with tax is $${pricewithTax}`;
+let finalMessage = `The final price after discount is $${finalPrice}`;
+
+console.log(taxmessage);
+console.log(finalMessage);
